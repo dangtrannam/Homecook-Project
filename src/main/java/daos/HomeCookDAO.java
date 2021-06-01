e1a86969139287790adbff4ff8e5a4e751cbe8c2
Peter-Parker12 <megapolis661@gmail.com>
2021-06-01 13:28:34 +0700

    Update of AccountDTO file. Added UserDAO and HomeCookDAO

---
+++ src/main/java/daos/HomeCookDAO.java
@@ -0,0 +1,18 @@
+package daos;
+
+import java.util.ArrayList;
+
+import dtos.Account;
+
+public class HomeCookDAO {
+	public ArrayList<Account> getAllHomeCook(){
+		try {
+			ArrayList<Account> result = new ArrayList<Account>();
+			 
+		}		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
